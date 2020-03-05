a0474381c542e6a0c16f15843e5c9f6b51bf0001
Uday Bansal <43824981+udaybansal19@users.noreply.github.com>
2020-03-05 20:15:41 +0530

    func

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int cnt=0;
+double r=0.01;
+
+double func(double x) {
+
+    return r*x*(1-x);
+
+}
+
+int rando() {
+
+    
+
+    return ;
+}
+
+int main() {
+
+
+    return 0;
+}
