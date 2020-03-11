2e805358f83b407ae770f7d7eca387ba2258ca6e
Uday Bansal <43824981+udaybansal19@users.noreply.github.com>
2020-03-11 22:51:50 +0530

    Something wrong with increase rate

--- main.cpp
+++ main.cpp
@@ -2,24 +2,33 @@
 
 using namespace std;
 
-int cnt=0;
-double r=0.01;
+int itr=0;
 
-double func(double x) {
+double func(double x, double r) {
 
     return r*x*(1-x);
 
 }
 
-int rando() {
+double rando(double ini, double r ) {
 
-    
+    double x = ini;
 
-    return ;
+    for(int i=0;i<itr;i++){
+        x = func(x,r);
+        cout<<x<<endl;
+    }
+
+
+    return x;
 }
 
 int main() {
 
+    cout << fixed << setprecision(6);
+
+    itr = 10;
+    cout<<rando(1,0.2);
 
     return 0;
 }
