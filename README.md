fb7978f4783546fe979f15e9e712c9c5e595c045
Uday Bansal <43824981+udaybansal19@users.noreply.github.com>
2020-04-07 05:38:19 +0530

    Readme and logistic map images

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Rando
-Algorithm to generate random numbers using Mandelbrot set
+Repository to experiment with different pseudo random generating algorithms.
+
+## Rando
+Implementation of Logistics map in python and c++
+
+
+## Chaos Game
+Method to check if a sequence of numbers is random or not.
--- chaos-game.py
+++ chaos-game.py
@@ -9,6 +9,9 @@ numberOfPoints = 100000
 # Prime numbers didn't work
 # Rando works!!!
 #   Gives somewhat shape for r=3 as well
+#
+#   Value above bifurcation r=3.6 to r=4 gives perfect fractal
+#   with disintegration below it
 # TODO: Digits of pi and other irrational numbers
 
 x_coor = arr.array('d')
@@ -18,19 +21,6 @@ x = 1
 y = 2
 c = 0
 
-randNum = arr.array('d')
-growthRate = arr.array('d')
-
-def func(x,r):
-  return r*x*(1-x)
-
-def rand_func():
-    eff = 100
-    global c
-    for i in range(3.6*eff,4*eff):
-        rando(x,i/eff)
-    c = c+1
-    return randNum[c]*1000
 
 def next_point(z):
 
@@ -53,11 +43,8 @@ def next_point(z):
 
 def chaos_game(x,y):
 
-    x = 0.2
     for i in range(0,numberOfPoints):
-        x = func(x,4)
         next_point((int)(x*1000))
-
 
 
 def plotIT():
