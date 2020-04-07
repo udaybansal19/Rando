a13f845372c8392d5fa563fdd03cfc9561fd1c69
Uday Bansal <43824981+udaybansal19@users.noreply.github.com>
2020-04-07 05:43:01 +0530

    Images of maps and graphs

--- chaos-game.py
+++ chaos-game.py
@@ -21,6 +21,19 @@ x = 1
 y = 2
 c = 0
 
+randNum = arr.array('d')
+growthRate = arr.array('d')
+
+def func(x,r):
+  return r*x*(1-x)
+
+def rand_func():
+    eff = 100
+    global c
+    for i in range(3.6*eff,4*eff):
+        rando(x,i/eff)
+    c = c+1
+    return randNum[c]*1000
 
 def next_point(z):
 
@@ -43,17 +56,20 @@ def next_point(z):
 
 def chaos_game(x,y):
 
+    x = 0.2
     for i in range(0,numberOfPoints):
+        x = func(x,4)
         next_point((int)(x*1000))
+
 
 
 def plotIT():
 	
 	plt.plot(x_coor,y_coor,'.',color='blue') 
   
-	plt.ylabel('Random number')  
-	plt.xlabel('Seed')  
-	plt.title('Random number with corresponding seed') 
+	# plt.ylabel('Random number')  
+	# plt.xlabel('Seed')  
+	plt.title('Chaos Game Plotted with values from Logistics equation') 
   
 	plt.show()
 
